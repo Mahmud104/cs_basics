1b537c76bc33a9c00576921e930a0a8dd1b47894
Mahmud104 <abigailkyalo104@gmail.com>
2023-01-19 16:53:46 +0300

    hello 3*

--- hello.c
+++ hello.c
@@ -1,5 +1,9 @@
 #include<stdio.h>
 
 void main(){
-    printf("Hello  world");
+    printf("Hello  world \n");
+
+    printf("Hello  world \n");
+    printf("Hello  world \n");
+    printf("Hello  world \n");
 }
